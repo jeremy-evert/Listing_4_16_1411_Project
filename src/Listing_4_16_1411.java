3e4d95eea035483abe2954c37a42507358c305af
Jeremy Evert <jeremy.evert@swosu.edu>
2015-11-04 09:48:00 -0600

    have some nice code

--- src/Listing_4_16_1411.java
+++ src/Listing_4_16_1411.java
@@ -1,16 +1,15 @@
 public class Listing_4_16_1411 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		
-		// A was 65
-		// Z was 90
-		// we want to start at 65 and go to 65 + 
-		// a is 65
-		// b is going to be 25
-		// Copied from Brennon Cupp's previous work: work_class.java
-		int number = (int)(65 + Math.random()*(25+1));
-		System.out.println(number);
-		char letter = (char)number;
-		System.out.print(letter);
+		// uppercase letters
+		int startingValue = 65; // A was 65, our starting value
+		int endingValue = 90 - 65 + 1; 	/*Z was 90
+		* but Math.random excludes the upper limit, so we added one 
+		* more to ensure it included Z in the possible outputs.*/
+		
+		/* Math.random example copied from Brennon Cupp's 
+		 * previous work: work_class.java */
+	
+		System.out.println((char)(startingValue + Math.random()*(endingValue)));
 	}
 }
