1d4f2389a35c607dcfb2143a3f791473d4d7eb0b
Jeremy Evert <jeremy.evert@swosu.edu>
2015-11-04 09:31:31 -0600

    Have some working code

--- src/Listing_4_16_1411.java
+++ src/Listing_4_16_1411.java
@@ -1,5 +1,16 @@
 public class Listing_4_16_1411 {
 	public static void main(String[] args) {
-		System.out.print("Hello");
+		System.out.println("Hello");
+		
+		// A was 65
+		// Z was 90
+		// we want to start at 65 and go to 65 + 
+		// a is 65
+		// b is going to be 25
+		// Copied from Brennon Cupp's previous work: work_class.java
+		int number = (int)(65 + Math.random()*(25+1));
+		System.out.println(number);
+		char letter = (char)number;
+		System.out.print(letter);
 	}
 }
