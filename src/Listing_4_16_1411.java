eee1f127f6042efd71a68e8072eb91e58df68985
Jeremy Evert <jeremy.evert@swosu.edu>
2015-11-06 09:45:25 -0600

    Added a testing method.

--- src/Listing_4_16_1411.java
+++ src/Listing_4_16_1411.java
@@ -1,15 +1,50 @@
 public class Listing_4_16_1411 {
 	public static void main(String[] args) {
 		
+		char letter = generateRandomCharacter();
+		
+		testGenerateRandomCharacter();
+	
+		System.out.print("\nHowdy\n");
+		System.out.println(letter);
+		
+	}
+	
+	public static char generateRandomCharacter(){
 		// uppercase letters
 		int startingValue = 65; // A was 65, our starting value
 		int endingValue = 90 - 65 + 1; 	/*Z was 90
 		* but Math.random excludes the upper limit, so we added one 
-		* more to ensure it included Z in the possible outputs.*/
+		* more to ensure it included Z in the possible outputs.
+		* More information on an ASCII table.*/
 		
 		/* Math.random example copied from Brennon Cupp's 
-		 * previous work: work_class.java */
-	
-		System.out.println((char)(startingValue + Math.random()*(endingValue)));
+		 * previous work: work_class.java 
+		 * More information from the Java text in 
+		 * section 4.2.5, the random method.*/
+		char letterRandom = (char)(startingValue + Math.random()*(endingValue));
+		//System.out.print(letterRandom);
+		return letterRandom;
+		
+	}
+	public static void testGenerateRandomCharacter(){
+		
+		for (int i = 65; i<=90; i++){
+			System.out.println("\nOur letter is: " + (char)i);
+			boolean haveAMatch = false;
+			while(!haveAMatch){
+				int letter = generateRandomCharacter();
+				System.out.print((char)letter);
+				
+				if(letter == i){
+					haveAMatch = true;
+				}
+				
+			}
+			
+		}
+		
+		
+		
 	}
 }
---
+++ src/Listing_4_16_Method_and_Test.java
@@ -0,0 +1,6 @@
+public class Listing_4_16_Method_and_Test {
+	public static void main(String[] args) {
+		System.out.print("Howdy");
+
+	}
+}
